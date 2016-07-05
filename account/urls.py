7e5bac5db25e31dc4da531360d53a8dda55f97ce
Abhisheksoni1 <abhisheksoni500@gmail.com>
2016-07-05 16:37:48 +0530

    Initial commit

---
+++ account/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+# post views
+
+        #url(r'^login/$', views.user_login, name='login'),
+        url(r'^login/$', 'django.contrib.auth.views.login',name='login'),
+        url(r'logout/$','django.contrib.auth.view.login',name='logout'),
+        url(r'^logout-then-login/$','django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+        url(r'^$', views.dashboard, name='dashboard'),
+]
