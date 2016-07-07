e433a6466c354dbd425f410d12e69dfe347eb8bb
Abhisheksoni1 <abhisheksoni500@gmail.com>
2016-07-07 20:09:12 +0530

    social authentication added to the site

--- account/authentication.py
+++ account/authentication.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 
+
 class EmailAuthBackend(object):
     """
     Authenticate using e-mail account.
     """
+
     def authenticate(self, username=None, password=None):
         try:
             user = User.objects.get(email=username)
@@ -12,6 +14,7 @@ class EmailAuthBackend(object):
             return None
         except User.DoesNotExist:
             return None
+
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
